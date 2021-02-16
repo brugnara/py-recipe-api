678d45a57858e6c64d31c2f753455bb0347cd53a
Daniele Brugnara <daniele@brugnara.me>
2021-02-16 15:15:29 +0100

    recipe

--- app/core/admin.py
+++ app/core/admin.py
@@ -54,3 +54,4 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Tag)
+admin.site.register(models.Ingredient)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 from recipe import serializers
 
 
@@ -24,3 +24,16 @@ class TagViewSet(viewsets.GenericViewSet,
     def perform_create(self, serializer):
         """creates a new tag"""
         serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin):
+    """manage"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """curr user objs"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
